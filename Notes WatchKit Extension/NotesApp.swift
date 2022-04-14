63790122d0b36a8f38e93c982c84cf0f28d8ab2e
newlandslucas <lucasnewlands.m@gmail.com>
2022-04-14 10:32:07 -0300

    initial configuration

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Lucas Newlands on 14/04/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
